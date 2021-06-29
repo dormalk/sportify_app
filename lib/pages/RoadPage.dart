672871e4bb89c58d57543b8a0978947375eca9b7
Dor Malka <dormalk@gmail.com>
2021-06-29 08:34:36 +0300

    init app

---
+++ lib/pages/RoadPage.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class RoadPage extends StatelessWidget {
+  const RoadPage({Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Center(
+        child: Text('Road'),
+      ),
+    );
+  }
+}
---
+++ lib/widgets/MainNavigation.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+import 'package:sportify_app/pages/FeedPage.dart';
+import 'package:sportify_app/pages/MapPage.dart';
+import 'package:sportify_app/pages/RoadPage.dart';
+
+class MainNavigation extends StatefulWidget {
+  const MainNavigation({Key key}) : super(key: key);
+
+  @override
+  _MainNavigationState createState() => _MainNavigationState();
+}
+
+class _MainNavigationState extends State<MainNavigation> {
+  int _currentIndex = 0;
+  final tabs = [MapPage(), RoadPage(), FeedPage()];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: tabs[_currentIndex],
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: _currentIndex,
+        type: BottomNavigationBarType.shifting,
+        items: [
+          BottomNavigationBarItem(
+              icon: Icon(Icons.map),
+              backgroundColor: Colors.blue,
+              label: 'Map'),
+          BottomNavigationBarItem(
+              icon: Icon(Icons.person),
+              backgroundColor: Colors.green,
+              label: 'Road'),
+          BottomNavigationBarItem(
+              icon: Icon(Icons.post_add),
+              backgroundColor: Colors.red,
+              label: 'Feed'),
+        ],
+        onTap: (index) {
+          setState(() {
+            _currentIndex = index;
+          });
+        },
+      ),
+    );
+  }
+}
