0efe6b123694566808a85750b2ef981b3939343b
dormalka2019 <dormalk@gmail.com>
2021-08-09 19:10:04 +0300

    refactor trackerInfo

--- lib/widgets/MapPageWidgets/TrackerInformationCard.dart
+++ lib/widgets/MapPageWidgets/TrackerInformationCard.dart
@@ -4,48 +4,7 @@ import 'package:sportify_app/providers/Tracker.dart';
 import 'package:provider/provider.dart';
 import 'package:sportify_app/shared/Labels.dart';
 
-class TrackerInformationCard extends StatefulWidget {
-  const TrackerInformationCard({Key key}) : super(key: key);
-
-  @override
-  _TrackerInformationCardState createState() => _TrackerInformationCardState();
-}
-
-class _TrackerInformationCardState extends State<TrackerInformationCard> {
-  String _timeFromStart;
-  String _distance;
-  String _velocity;
-  String _caloriesBurn;
-
-  IconData _activityIcon;
-
-  @override
-  void initState() {
-    _activityIcon = mapActivityIcon[Provider.of<Tracker>(context, listen: false)
-        .currentActivity
-        ?.activityType];
-    super.initState();
-  }
-
-  void _listen() {
-    setState(() {
-      try {
-        _timeFromStart =
-            Provider.of<Tracker>(context, listen: true).fommatedTimer;
-
-        _distance = Provider.of<Tracker>(context, listen: true)
-            .totalDistanceInKm
-            .toStringAsFixed(2);
-        _velocity = Provider.of<Tracker>(context, listen: true)
-            .velocity
-            .toStringAsFixed(2);
-        _caloriesBurn = Provider.of<Tracker>(context, listen: true)
-            .totalCaloriesBurn
-            .toStringAsFixed(2);
-      } catch (e) {}
-    });
-  }
-
+class TrackerInformationCard extends StatelessWidget {
   Widget _buildCol({int flex, String value, String label, double fontSize}) {
     return Expanded(
       flex: flex,
@@ -65,39 +24,49 @@ class _TrackerInformationCardState extends State<TrackerInformationCard> {
   }
 
   Widget _buildDurationCol() {
-    return Row(
-      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-      children: [
-        Expanded(
-            flex: 1,
-            child: Icon(
-              _activityIcon,
-              size: 40,
-            )),
-        _buildCol(
-            flex: 2,
-            value: _timeFromStart,
-            label: Labels.duration,
-            fontSize: 40),
-        Expanded(flex: 1, child: Container())
-      ],
+    return Consumer<Tracker>(
+      builder: (context, info, _) => Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          Expanded(
+              flex: 1,
+              child: Icon(
+                mapActivityIcon[info.currentActivity?.activityType],
+                size: 40,
+              )),
+          _buildCol(
+              flex: 2,
+              value: info.fommatedTimer,
+              label: Labels.duration,
+              fontSize: 40),
+          Expanded(flex: 1, child: Container())
+        ],
+      ),
     );
   }
 
   Widget _buildSecondRowCardInfo() {
-    return Row(
-      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-      children: [
-        _buildCol(
-            flex: 1,
-            value: _caloriesBurn,
-            label: Labels.calories,
-            fontSize: 25),
-        _buildCol(
-            flex: 1, value: _velocity, label: Labels.velocity, fontSize: 25),
-        _buildCol(
-            flex: 1, value: _distance, label: Labels.distance, fontSize: 25),
-      ],
+    return Consumer<Tracker>(
+      builder: (context, info, _) => Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          _buildCol(
+              flex: 1,
+              value: info.totalCaloriesBurn.toStringAsFixed(2),
+              label: Labels.calories,
+              fontSize: 25),
+          _buildCol(
+              flex: 1,
+              value: info.velocity.toStringAsFixed(2),
+              label: Labels.velocity,
+              fontSize: 25),
+          _buildCol(
+              flex: 1,
+              value: info.totalDistanceInKm.toStringAsFixed(2),
+              label: Labels.distance,
+              fontSize: 25),
+        ],
+      ),
     );
   }
 
@@ -113,7 +82,6 @@ class _TrackerInformationCardState extends State<TrackerInformationCard> {
 
   @override
   Widget build(BuildContext context) {
-    _listen();
     return Card(
       clipBehavior: Clip.antiAlias,
       color: Colors.white,
