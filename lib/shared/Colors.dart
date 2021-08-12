3bb06a926041a9be058edf392a0589b7cd60a441
dormalka2019 <dormalk@gmail.com>
2021-08-12 22:07:32 +0300

    change activity picker

---
+++ lib/shared/Colors.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+
+class CustomColor {
+  static final Color primaryColor = Color(0XFF000033);
+  static final Color seconderyColor = Color(0XFF1F1F60);
+}
--- lib/widgets/MapPageWidgets/FloatingStartButton.dart
+++ lib/widgets/MapPageWidgets/FloatingStartButton.dart
@@ -1,33 +1,132 @@
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:sportify_app/modals/Activity.dart';
 import 'package:sportify_app/providers/TrackerInfo.dart';
 import 'package:provider/provider.dart';
-import 'package:sportify_app/widgets/MapPageWidgets/CreateActivityModal.dart';
+import 'package:sportify_app/shared/Colors.dart';
 
-class FloatingStartButton extends StatelessWidget {
+class FloatingStartButton extends StatefulWidget {
   FloatingStartButton({Key key});
 
-  void _showDialog(BuildContext context) {
-    showDialog(
-        context: context,
-        builder: (BuildContext context) {
-          return CreateActivityModal();
-        });
+  @override
+  _FloatingStartButtonState createState() => _FloatingStartButtonState();
+}
+
+class _FloatingStartButtonState extends State<FloatingStartButton>
+    with SingleTickerProviderStateMixin {
+  bool isOpened = false;
+  AnimationController _animationController;
+  Animation<double> _animationIcon;
+  Animation<Color> _animationColor;
+  Animation<double> _translateButton;
+  Curve _curve = Curves.easeOut;
+  double _fabHeight = 56.0;
+
+  @override
+  initState() {
+    _animationController =
+        AnimationController(vsync: this, duration: Duration(milliseconds: 500))
+          ..addListener(() {
+            setState(() {});
+          });
+    _animationIcon =
+        Tween<double>(begin: 0.0, end: 1.0).animate(_animationController);
+    _animationColor = ColorTween(
+      begin: CustomColor.seconderyColor,
+      end: Colors.red,
+    ).animate(CurvedAnimation(
+      parent: _animationController,
+      curve: Interval(
+        0.00,
+        1.00,
+        curve: Curves.linear,
+      ),
+    ));
+    _translateButton = Tween<double>(
+      begin: _fabHeight,
+      end: -14.0,
+    ).animate(CurvedAnimation(
+      parent: _animationController,
+      curve: Interval(
+        0.0,
+        0.75,
+        curve: _curve,
+      ),
+    ));
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    _animationController.dispose();
+    super.dispose();
+  }
+
+  void animate() {
+    if (!isOpened)
+      _animationController.forward();
+    else
+      _animationController.reverse();
+    isOpened = !isOpened;
+  }
+
+  Widget _buildIconButton(ActivityType activityType) {
+    return Container(
+      child: FloatingActionButton(
+        elevation: 2,
+        onPressed: () {
+          _animationController.reverse().then((value) =>
+              Provider.of<TrackerInfo>(context, listen: false)
+                  .setActivity(activityType)
+                  .playActivity());
+        },
+        tooltip: 'Bike',
+        backgroundColor: Colors.grey[200],
+        child: Icon(mapActivityIcon[activityType], color: Colors.black),
+      ),
+    );
+  }
+
+  Widget toggle() {
+    return Container(
+      child: FloatingActionButton(
+        elevation: 2,
+        backgroundColor: _animationColor.value,
+        onPressed: animate,
+        tooltip: 'Toggle',
+        child: AnimatedIcon(
+          icon: AnimatedIcons.menu_close,
+          progress: _animationIcon,
+        ),
+      ),
+    );
   }
 
   @override
   Widget build(BuildContext context) {
-    bool _recordIsActive =
-        Provider.of<TrackerInfo>(context, listen: true).recordIsActive;
-    return !_recordIsActive
-        ? FloatingActionButton(
-            elevation: 1,
-            backgroundColor: Color(0XFF000033),
-            child: Icon(
-              Icons.add,
-              color: Colors.white,
-            ),
-            onPressed: () => _showDialog(context))
+    return !Provider.of<TrackerInfo>(context, listen: true).recordIsActive
+        ? Column(
+            mainAxisAlignment: MainAxisAlignment.end,
+            children: <Widget>[
+              Transform(
+                transform: Matrix4.translationValues(
+                  0.0,
+                  _translateButton.value * 2.0,
+                  0.0,
+                ),
+                child: _buildIconButton(ActivityType.Run),
+              ),
+              Transform(
+                transform: Matrix4.translationValues(
+                  0.0,
+                  _translateButton.value,
+                  0.0,
+                ),
+                child: _buildIconButton(ActivityType.Bike),
+              ),
+              toggle(),
+            ],
+          )
         : Container();
   }
 }
